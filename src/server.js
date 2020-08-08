f8bd4ea13c3550faa8f10e2e855e5042b7bfea7a
01ch01 <00claudio.henrique@gmail.com>
2020-08-08 04:43:04 -0300

    const proffys

--- src/server.js
+++ src/server.js
@@ -1,3 +1,17 @@
+const proffys = [
+  {
+    name: 'Diego Fernandes',
+    avatar: 'https://avatars2.githubusercontent.com/u/2254731?s=400&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    whatsapp:'',
+    bio:'Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por umas das minhas explosões.',
+    subject:'Química',
+    cost:'20',
+    weekday:'',
+    time_from:'',
+    time_to:'',
+  },
+];
+
 const express = require('express');
 const server = express();
 
@@ -7,8 +21,13 @@ server
   .get('/', (req, res) => {
     return res.sendFile(__dirname + '/views/index.html');
   })
+
   .get('/study', (req, res) => {
     return res.sendFile(__dirname + '/views/study.html');
+  })
+
+  .get('/give-classes', (req, res) => {
+    return res.sendFile(__dirname + '/views/give-classes.html');
   })
 
   .listen(5500);
